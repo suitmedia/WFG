753bb95f7a05569076ee037b0b403003792e45d6
AndhikaR <andhikaridhovani@gmail.com>
2016-04-12 14:49:15 +0700

    add index.js and package.json

---
+++ index.js
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+
+const fs = require ("fs");
+const path = require ("path");
+const shell = require ("shelljs");
+
+var content = '';
+
+fs.readdir('./',function(err,files){
+    if (err) throw err;
+    console.log('On process');
+    
+    files.forEach(function(file){
+    	if (file.includes('.ttf')) {
+            var filename = path.basename(file,'.ttf');
+            var familyName = filename.split('-');
+           
+            shell.exec('ttf2woff '+ filename +'.ttf '+ filename +'.woff');
+            shell.exec('ttf2eot '+ filename +'.ttf '+ filename +'.eot');
+            console.log(`Converting ${filename}`);
+            content += stylesheetContent (familyName,filename);
+        }
+    });
+    
+    fs.writeFile('stylesheet.css', content , (err) => {
+        if (err) throw err;
+    });
+    console.log('Convert finish');
+});
+
+function stylesheetContent (familyName,filename) {
+    return `@font-face {
+    font-family: '${familyName[0]}';
+    src: url('${filename}.eot');
+    src: url('${filename}.eot?#iefix') format('embedded-opentype'),
+    url('${filename}.woff') format('woff'),
+    url('${filename}.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal; 
+}
+
+`
+}
--- readme.markdown
+++ readme.markdown
@@ -1,1 +1,17 @@
-# WebFont generator
+#Font generator ttf2eot&woff
+
+
+###Installation
+1.Clone repo ini
+
+2.`cd` ke directory repo yang sudah di clone 
+
+3.Jalankan command `npm run setup` untuk menginstall module 
+
+
+###Cara Penggunaan
+1.`cd` ke direktori `.ttf` file yang ingin di convert
+
+2.Jalankan command `font-generator` untuk mengconvert file
+
+3.Hasil convert bisa di check di lokal direktori 
