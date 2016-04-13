c6f1a5ba9ab9ea2c7ebc03a80a7c8438dadf79ac
AndhikaR <andhikaridhovani@gmail.com>
2016-04-13 16:29:39 +0700

    add ttfinfo as dependencies and code clean up

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ const ttfInfo = require('ttfinfo');
 
 var content = '';
 
-fs.readdir('./', function(err,files) {
+fs.readdir('./', function( err,files ) {
     if (err) throw err;
 
     console.log('On process');
 
-    var TTFs = files.filter( function(file) {
+    var TTFs = files.filter( function( file ) {
         return file.includes('.ttf');
-    }) 
+    }); 
 
     var length = TTFs.length;
     var counter = 0;
     
-    TTFs.forEach( function(file) {
+    TTFs.forEach( function( file ) {
         var fileName = path.basename(file,'.ttf');
         var familyName = fileName.split('-');
 
-        ttfInfo(`${fileName}.ttf`, function(err, info) {
+        ttfInfo(`${fileName}.ttf`, function( err, info ) {
             if (err) throw err;
             var fontWeight = (info.tables['OS\/2'].weightClass);
             var fontInfo = (info.tables.post.italicAngle);
@@ -41,25 +41,25 @@ fs.readdir('./', function(err,files) {
             shell.exec(`ttf2eot ${fileName}.ttf ${fileName}.eot`);
             shell.exec(`cat ${fileName}.ttf | ttf2woff2 >> ${fileName}.woff2`);
             
-            content += stylesheetContent (familyName,fileName,fontWeight,fontStyle);
+            content += stylesheetContent (familyName[0], fileName, fontWeight, fontStyle);
             counter++;
 
             if (counter === length) {
                 fs.writeFile('stylesheet.css', content , (err) => {
                     if (err) throw err;
-                });
 
-                console.log('Convert finish');
+                    console.log('Convert finish');
+                });   
             }
         }); 
     });
 });
 
-function stylesheetContent (familyName,fileName,fontWeight,fontStyle) {
+function stylesheetContent( familyName, fileName, fontWeight, fontStyle ) {
 
     return `
 @font-face {
-    font-family: '${familyName[0]}';
+    font-family: '${familyName}';
     src: url('${fileName}.eot');
     src: url('${fileName}.eot?#iefix') format('embedded-opentype'),
          url('${fileName}.woff2') format('woff2'),
